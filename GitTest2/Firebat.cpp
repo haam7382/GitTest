2885445748dd7be3fb987019389bc09c0e417ae0
DESKTOP-7FL9E6G\User <dhgmldhks@gmail.com>
2024-04-02 17:31:46 +0900

    Revert "코드 오류 수정"
    This reverts commit ea7f367883b72bb276002c50d055826eba387516.

--- GitTest2/Firebat.cpp
+++ GitTest2/Firebat.cpp
@@ -14,7 +14,7 @@ void Firebat::doPowerStatus() {
 	printf("파이어뱃의 공격력은 %d 입니다\n", power);
 }
 
-void Firebat::strongAttack(int n)
+void strongAttack(int n)
 {
 	printf("%d의 공격력으로 강하게 공격합니다.\n", power + n);
 }
